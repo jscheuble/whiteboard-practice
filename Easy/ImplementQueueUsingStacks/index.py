c04c151ae2ae067bf13fa7250801610cdcbf0fd9
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-09-01 19:16:51 -0700

    implement queue using stacks

---
+++ Easy/ImplementQueueUsingStacks/index.py
@@ -0,0 +1,58 @@
+'''
+Implement the following operations of a queue using stacks.
+
+push(x) -- Push element x to the back of queue.
+pop() -- Removes the element from in front of queue.
+peek() -- Get the front element.
+empty() -- Return whether the queue is empty.
+Example:
+
+MyQueue queue = new MyQueue();
+
+queue.push(1);
+queue.push(2);  
+queue.peek();  // returns 1
+queue.pop();   // returns 1
+queue.empty(); // returns false
+Notes:
+
+You must use only standard operations of a stack -- which means only push to top, peek/pop from top, size, and is empty operations are valid.
+Depending on your language, stack may not be supported natively. You may simulate a stack by using a list or deque (double-ended queue), as long as you use only standard operations of a stack.
+You may assume that all operations are valid (for example, no pop or peek operations will be called on an empty queue).
+'''
+
+
+class MyQueue:
+
+    def __init__(self):
+        self.stack = []
+        self.stack2 = []
+        
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+        
+
+    def pop(self) -> int:
+        self.peek()
+        return self.stack2.pop()
+        
+
+    def peek(self) -> int:
+        if not self.stack2:
+            while self.stack:
+                self.stack2.append(self.stack.pop())
+        return self.stack2[-1]
+        
+
+    def empty(self) -> bool:
+        return not self.stack and not self.stack2
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
