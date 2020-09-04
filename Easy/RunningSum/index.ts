4c0ebd2aec1733a729ed2b7c9a7a2cdd8ff6b5f8
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-09-04 13:42:31 -0700

    running sum of 1d array ts

---
+++ Easy/RunningSum/index.ts
@@ -0,0 +1,44 @@
+/*
+
+1480. Running Sum of 1d Array
+
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+Return the running sum of nums.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [1,3,6,10]
+Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+Example 2:
+
+Input: nums = [1,1,1,1,1]
+Output: [1,2,3,4,5]
+Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+Example 3:
+
+Input: nums = [3,1,2,10,1]
+Output: [3,4,6,16,17]
+
+Constraints:
+
+1. 1 <= nums.length <= 1000
+2. -10^6 <= nums[i] <= 10^6
+*/
+
+function runningSum(nums: number[]): number[] {
+    let count: number = 0;
+    let result: number[] = nums.map((num, i) => {
+        if (i === 0) {
+            count = num
+            return num;
+        } else {
+            count = count + num
+            return count;
+        }
+    })
+    return result;
+};
